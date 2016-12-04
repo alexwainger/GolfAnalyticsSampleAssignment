62c6cb0f8f89156b0f047e911cd4efd67d163851
Alex Wainger <alex_wainger@brown.edu>
2016-12-03 20:28:07 -0500

    Cleaned up, created functions, need to comment

--- part2/script.m
+++ part2/script.m
@@ -1,19 +1,37 @@
 %%% Golf Analytics Sample Assignment Part 2
 %%% Alex Wainger
-%%
+%% Constants
 file_name = 'round-2014-small.txt';
-column_names = {'RoundScore', 'GIRRank', 'OverallPuttingAvg__OfPutts_'};
-
-T = readtable(file_name, 'Delimiter', ';', 'ReadVariableNames', 1, 'ReadRowNames', 0);
-
-%%
-
 modelspec = 'RoundScore ~ GIRRank + OverallPuttingAvg__OfPutts_';
-model = fitlm(T,modelspec);
+mc_column_name = 'RoundScore';
+mc_iters = 10000;
+mc_sample_size = 4;
+mc_threshold = 270;
 
-%%
-scores = transpose(table2array(columns_of_interest(:, 1)));
-rng(1);
-random_mat = randi(numel(scores), 10000, 4);
-samples = scores(random_mat);
-sum(sum(samples, 2) <= 270) / numel(scores);
+%% Read File
+
+T = file_to_table(file_name);
+
+%% Linear Regression
+model = linear_regression(T, modelspec);
+
+%% Monte Carlo
+vector = transpose(table2array(T(:, mc_column_name)));
+percentage = monte_carlo(vector, mc_iters, mc_sample_size, mc_threshold);
+
+%% Functions
+
+function T = file_to_table(f_name)
+    T = readtable(f_name, 'Delimiter', ';', 'ReadVariableNames', 1, 'ReadRowNames', 0);
+end
+
+function model = linear_regression(table, spec)
+    model = fitlm(table,spec);
+end
+
+function percentage = monte_carlo(vector, iters, sample_size, threshold)
+    rng(1);
+    random_mat = randi(numel(vector), iters, sample_size);
+    samples = vector(random_mat);
+    percentage = sum(sum(samples, 2) <= threshold) / numel(vector);
+end
